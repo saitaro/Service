ba8a7d008a7bc1f37f70745c13ee17f2d1375497
Vici <ripsodi@gmail.com>
2019-02-10 05:54:35 +0300

    Attach PostgreSQL to Docker image

--- Service/settings.py
+++ Service/settings.py
@@ -85,19 +85,19 @@ WSGI_APPLICATION = "Service.wsgi.application"
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     # "ENGINE": "django.db.backends.postgresql",
-    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    #     "NAME": "postgres",
-    #     "USER": "postgres",
-    #     "PASSWORD": "nelson",
-    #     "HOST": "localhost",
-    #     "PORT": "5432",
-    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        # "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "nelson",
+        "HOST": "db",
+        "PORT": "5432",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    # }
 }
 
 
