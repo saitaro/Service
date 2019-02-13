688036967c46dbc3154dd6393ac4f1cbf1010597
Vici <ripsodi@gmail.com>
2019-02-13 23:18:39 +0300

    Configure db in Docker image

--- CRM/serializers.py
+++ CRM/serializers.py
@@ -8,26 +8,26 @@ from django.utils import timezone
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(default="defaultpass")
+    password = serializers.CharField(default='defaultpass')
 
     class Meta:
         model = User
-        fields = "url", "username", "email", "groups", "password"
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = 'url', 'username', 'email', 'groups', 'password'
+        extra_kwargs = {'password': {'write_only': True}}
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
-        fields = "__all__"
+        fields = '__all__'
 
 
 class OptionsSerializer(serializers.Serializer):
-    master = serializers.CharField(source="master__user__username")
+    master = serializers.CharField(source='master__user__username')
     price = serializers.IntegerField()
 
     class Meta:
-        fields = "master", "price"
+        fields = 'master', 'price'
 
 
 class SkillSerializer(serializers.ModelSerializer):
@@ -35,59 +35,66 @@ class SkillSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Skill
-        fields = "id", "name", "options", "average_price"
+        fields = 'id', 'name', 'options', 'average_price'
 
 
 class CatalogSerializer(serializers.Serializer):
-    skill = serializers.CharField(source="skill__name")
+    skill = serializers.CharField(source='skill__name')
     price = serializers.IntegerField()
 
     class Meta:
-        fields = "skill", "price"
+        fields = 'skill', 'price'
 
 
 class MasterSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(source="user.username", read_only=True)
+    name = serializers.CharField(source='user.username', read_only=True)
     catalog = CatalogSerializer(many=True)
     average_price = serializers.IntegerField()
 
     class Meta:
         model = Master
-        fields = "id", "name", "catalog", "average_price"
+        fields = 'id', 'name', 'catalog', 'average_price'
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-    skill = serializers.CharField(source="skill.name")
-    master = serializers.CharField(source="master.user.username")
+    skill = serializers.CharField(source='skill.name')
+    master = serializers.CharField(source='master.user.username')
 
     class Meta:
         model = Service
-        fields = "id", "skill", "master", "price"
+        fields = 'id', 'skill', 'master', 'price'
 
 
 class OrderSerializer(serializers.ModelSerializer):
     client = serializers.CharField(
         default=serializers.CurrentUserDefault(), read_only=True
     )
-    client_id = serializers.IntegerField(source="client.id", read_only=True)
+    client_id = serializers.IntegerField(source='client.id', read_only=True)
     service = ServiceSerializer(read_only=True)
-    service_id = serializers.IntegerField(source="service.id")
+    service_id = serializers.IntegerField(source='service.id')
 
     class Meta:
         model = Order
-        fields = "client", "client_id", "service", "service_id", "execution_date"
+        fields = (
+            'client',
+            'client_id',
+            'service',
+            'service_id',
+            'execution_date',
+        )
 
     def create(self, validated_data):
-        service_id = validated_data.pop("service")["id"]
+        service_id = validated_data.pop('service')['id']
         try:
             service = Service.objects.get(pk=service_id)
         except Service.DoesNotExist:
             raise serializers.ValidationError(
                 code=status.HTTP_404_NOT_FOUND,
-                detail="No service with id %d provided." % service_id,
+                detail='No service with id %d provided.' % service_id,
             )
         order = Order.objects.create(
-            **validated_data, service=service, client=self.context["request"].user
+            **validated_data,
+            service=service,
+            client=self.context['request'].user
         )
         return order
-
